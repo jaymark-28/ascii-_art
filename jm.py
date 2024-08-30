cb279c74e4f4fef7bdd76b821a17368687095045
jaymark-28 <jaymarkf649@gmail.com>
2024-08-30 18:04:32 +0800

    Add files via upload

---
+++ jm.py
@@ -0,0 +1,11 @@
+import pyfiglet
+
+from pyfiglet import Figlet
+from termcolor import colored
+
+
+figlet = Figlet(font='larry3d') 
+ascii_art = figlet.renderText('jaymark')
+colored_art = colored(ascii_art, 'cyan')
+
+print(colored_art)
